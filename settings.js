19e163381b99973854c5004278cfd75d3e0be64f
sumith211 <sumithnew3@gmail.com>
2025-03-28 08:07:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'TITAN',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "TiTAN Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '918089741036', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
